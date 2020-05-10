f7ed3281da511c3467e392faceb6eec6ad173889
ceosss <sswarajsamant@gmail.com>
2020-05-10 22:59:10 +0530

    Updates

---
+++ precache-manifest.71b07c373a3096f3e06ef42491fbdd7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "805c909c77c799513803e433e5207c36",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "96a0bc54a459c7694190",
+    "url": "/react-monsters-rolodex/static/css/main.3f66a3fa.chunk.css"
+  },
+  {
+    "revision": "75388fd1a313f736f3e8",
+    "url": "/react-monsters-rolodex/static/js/2.fcd26ab3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters-rolodex/static/js/2.fcd26ab3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96a0bc54a459c7694190",
+    "url": "/react-monsters-rolodex/static/js/main.f0f94323.chunk.js"
+  },
+  {
+    "revision": "f6e91b1db0ad84e38d05",
+    "url": "/react-monsters-rolodex/static/js/runtime-main.f79a6e10.js"
+  }
+]);
